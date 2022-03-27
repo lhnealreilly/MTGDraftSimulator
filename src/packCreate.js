c670af84a351d6ceaf47a80fc8316a42fbb0e4ba
Trevor Ludlam <71052042+TrevorLudlam@users.noreply.github.com>
2022-03-26 21:16:30 -0400

    added timout

--- src/packCreate.js
+++ src/packCreate.js
@@ -24,17 +24,18 @@ async function getPack(){
     let pack = [];
     //land
     pack.push(await getCard("https://api.scryfall.com/cards/random?q=usd>%3D10+t%3Aland"));
+    
     //Foil Analog
-    pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28block%3Ahtr+or+set%3Acmb2+or+%28set%3Apcel+%28t%3Acreature+or+t%3Asummon%29%29+or+%28border%3Asilver+and+usd>%3D7%29%29+-is%3Atoken"));
+    setTimeout(pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28block%3Ahtr+or+set%3Acmb2+or+%28set%3Apcel+%28t%3Acreature+or+t%3Asummon%29%29+or+%28border%3Asilver+and+usd>%3D7%29%29+-is%3Atoken")), 20);
     //Rare Analog
-    pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28usd>%3D100+-is%3Atoken+-t%3Aland"));
+    setTimeout(pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28usd>%3D100+-is%3Atoken+-t%3Aland")), 20);
     //Uncommon Analog
     for(let i = 0; i < 3; ++i){
-        pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28usd>%3D10+-is%3Atoken+-t%3Aland"));
+        setTimeout(pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28usd>%3D10+-is%3Atoken+-t%3Aland")), 20);
     }
     //Common Analog
     for(let i = 0; i < 10; ++i){
-        pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28rarity%3Arare+or+rarity%3Amythic%29%28-is%3Atoken+-t%3Aland+-is%3Adigital+-set%3Apcel+-block%3Ahtr+-set%3Acmb2+-set%3Acmb1"));
+        setTimeout(pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28rarity%3Arare+or+rarity%3Amythic%29%28-is%3Atoken+-t%3Aland+-is%3Adigital+-set%3Apcel+-block%3Ahtr+-set%3Acmb2+-set%3Acmb1")), 20);
     }
 
     //for(let i = 0; i < 15; ++i){
