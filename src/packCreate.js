c33efbb34f90089f7f3060f2339ed1a3506c9ac2
Trevor Ludlam <71052042+TrevorLudlam@users.noreply.github.com>
2022-03-26 22:31:52 -0400

    no land pls

--- src/packCreate.js
+++ src/packCreate.js
@@ -39,7 +39,7 @@ async function getPack(){
     colorConsistency = colorCheck(colorConsistency, pack[curElem].color);
     curElem++;
     //Foil Analog
-    pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28block%3Ahtr+or+set%3Acmb2+or+%28set%3Apcel+%28t%3Acreature+or+t%3Asummon%29%29+or+%28border%3Asilver+and+usd>%3D7%29%29+-is%3Atoken"));
+    pack.push(await getCard("https://api.scryfall.com/cards/random?q=%28block%3Ahtr+or+set%3Acmb2+or+%28set%3Apcel+%28t%3Acreature+or+t%3Asummon%29%29+or+%28border%3Asilver+and+usd>%3D7%29%29+-is%3Atoken+-t%3Aland"));
     colorConsistency = colorCheck(colorConsistency, pack[curElem].color);
     curElem++;
     //Rare Analog
