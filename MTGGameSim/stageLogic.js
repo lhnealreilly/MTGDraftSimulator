b7350b5192f5bc5c7a5eeee8009edfc5a554c3e8
Liam Neal Reilly <lhnealreilly@gmail.com>
2022-03-30 16:11:08 -0400

    Added card zone logic

--- MTGGameSim/stageLogic.js
+++ MTGGameSim/stageLogic.js
@@ -6,30 +6,39 @@ var stage = new Konva.Stage({
     width: width,
     height: height,
 });
+stage.container().style.position = 'absolute';
+stage.container().style.top = '0';
+stage.container().style.left = '0';
 
 var layer = new Konva.Layer();
 stage.add(layer);
 
+const cardZones = document.getElementsByClassName("sub-card-zone");
+let cardZoneRects = [];
+for (zone of cardZones) {
+    cardZoneRects.push({ rect: zone.getBoundingClientRect(), cards: [] });
+}
+
 function addCard(url) {
     var imageObj = new Image();
+    let scale = getShortestZoneHeight()/3.5;
     imageObj.onload = function () {
         var card = new Konva.Image({
             x: 100,
             y: 250,
-            offsetX: 100,
-            offsetY: 200 * 3.5 / (2.5*2),
+            offsetX: scale*2.5/2,
+            offsetY: scale * 3.5 / 2,
             image: imageObj,
-            width: 200,
-            height: 200 * 3.5 / 2.5,
+            width: scale*2.5,
+            height: scale*3.5,
             draggable: true,
         });
-        let blockHeight = 200 * 3.5 / 2.5;
-        let blockWidth = 200;
+        card.on('dragstart', (e) => {
+            pickupCard(card);
+        });
         card.on('dragend', (e) => {
-            card.position({
-                y: Math.max(0+ card.offsetY(), Math.min(Math.round((card.y()-card.offsetY()) / blockHeight)  * blockHeight + card.offsetY(), Math.floor(height/blockHeight)*blockHeight)),
-                x: Math.max(0+ card.offsetX(), Math.min(Math.round((card.x()-card.offsetX()) / blockWidth)  * blockWidth + card.offsetX(), Math.floor(width/blockWidth)*blockWidth)),
-            });
+            console.log("Dropped");
+            dropCard(card);
             stage.batchDraw();
         });
         // add the shape to the layer
@@ -37,6 +46,55 @@ function addCard(url) {
     }
     imageObj.src = url;
 }
+
+
+function getShortestZoneHeight(){
+    let shortest = cardZoneRects[0].rect.height;
+    for (zone of cardZoneRects) {
+        if (zone.rect.height < shortest) {
+            shortest = zone.rect.height;
+        }
+    }
+    console.log(shortest);
+    return shortest;
+}
+
+function dropCard(card) {
+    for (zone of cardZoneRects) {
+        if (inRect(zone.rect, card)) {
+            zone.cards.push(card);
+            card.x(zone.rect.x + card.width() / 2);
+            card.y(zone.rect.y + card.height() / 2);
+            relayerZones();
+            return;
+        }
+    }
+}
+
+function pickupCard(card) {
+    for (zone of cardZoneRects) {
+        if (inRect(zone.rect, card)) {
+            if(zone.cards.includes(card)){
+                zone.cards.splice(zone.cards.indexOf(card), 1);
+            }
+            relayerZones();
+            return;
+        }
+    }
+}
+
+const yOffset = 40;
+function relayerZones(){
+    for(zone of cardZoneRects){
+        zone.cards.forEach((a, i) => {a.y(zone.rect.y + a.height() / 2 + i * yOffset); a.setZIndex(i)});
+    }
+}
+
+function inRect(rect, card) {
+    console.log(rect, card);
+    return (card.x() > rect.left && card.x() < rect.right && card.y() > rect.top && card.y() < rect.bottom);
+}
+
 
 
 let currentShape;
@@ -83,5 +141,5 @@ addCard("https://c1.scryfall.com/file/scryfall-cards/png/front/b/d/bd8fa327-dd41
 addCard("https://c1.scryfall.com/file/scryfall-cards/png/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.png?1614638838");
 addCard("https://c1.scryfall.com/file/scryfall-cards/png/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.png?1614638838");
 addCard("https://c1.scryfall.com/file/scryfall-cards/png/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.png?1614638838");
-addCard("https://c1.scryfall.com/file/scryfall-cards/png/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.png?1614638838");
+addCard("https://c1.scryfall.com/file/scryfall-cards/png/front/a/e/aea5c36b-c107-4daf-bedb-507b4cd64724.png?1643664067");
 
